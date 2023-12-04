6bcd6debee8a180f7d4327d186c88ab44b1e1af2
MutaiT <mutaitimo56@gmail.com>
2023-12-04 01:49:44 -0800

    Python Import Modules Task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    l = len(argv) - 1
-    if (l == 1):
-        print("{:d} argument:".format(l))
+    length = len(argv) - 1
+    if (length == 1):
+        print("{:d} argument:".format(length))
     else:
-        print("{:d} arguments{}".format(l, ":" if l != 0 else "."))
-    if (l > 0):
-        for num in range(1, l + 1):
+        print("{:d} arguments{}".format(length, ":" if length != 0 else "."))
+    if (length > 0):
+        for num in range(1, length + 1):
             print("{:d}: {}".format(num, argv[num]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    l = len(argv)
+    length = len(argv)
     total_sum = 0
-    if (l > 1):
-        for num in range(1, l):
+    if (length > 1):
+        for num in range(1, length):
             total_sum += int(argv[num])
     print("{:d}".format(total_sum))
