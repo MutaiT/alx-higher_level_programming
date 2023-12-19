7f672910093e07245b8a3067b6bf65726cdb0656
MutaiT <mutaitimo56@gmail.com>
2023-12-19 13:44:46 -0800

    Advanced Tasks

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""
+A python module defining a class Square with private attributes
+"""
+
+
+class Square:
+    """
+    A class Square defining an attribute size
+    """
+    def __init__(self, size=0):
+        """
+        initializes an instance attribute with given value
+        or zero if no value is given
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        getter function to get the value of the private attribute
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+        setter function to set the value of private attribute
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        finds the area of a square
+        """
+        return (self.__size ** 2)
+
+    def __lt__(self, sqr):
+        """
+        compares with less than operator
+        """
+        return self.area() < sqr.area()
+
+    def __le__(self, sqr):
+        """
+        compares with less than or equal to operator
+        """
+        return self.area() <= sqr.area()
+
+    def __eq__(self, sqr):
+        """
+        compares with equality operator
+        """
+        return self.area() == sqr.area()
+
+    def __ne__(self, sqr):
+        """
+        compares with not equal to operator
+        """
+        return self.area() != sqr.area()
+
+    def __gt__(self, sqr):
+        """
+        compares with greater than operator
+        """
+        return self.area() > sqr.area()
+
+    def __ge__(self, sqr):
+        """
+        compares with greater than or equal to operator
+        """
+        return self.area() >= sqr.area()
