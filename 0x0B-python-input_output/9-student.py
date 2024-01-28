2588fe6946caf1e2dd98d8569dd41412aa37e133
MutaiT <mutaitimo56@gmail.com>
2024-01-28 09:51:16 -0800

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+A module that defines a class Student
+"""
+
+
+class Student:
+    """
+    A class that  defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes the instance attributes
+        Args:
+            first_name: first name of the student
+            last_name: last name of the student
+            age: the age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Instance method that returns the dict properties of the object
+        """
+        return (self.__dict__)
